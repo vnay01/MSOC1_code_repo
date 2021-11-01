0620bf61b693a7c6338fdaf025a3e104cc0036f9
Vinay Singh <vnay01@gmail.com>
2021-11-01 10:48:37 +0100

    correct_files

---
+++ EITF35_Lab3_ALU/Lab3/Dev/DesignSources/ALU_tb.vhd
@@ -0,0 +1,80 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity tb_ALU is
+end tb_ALU;
+
+architecture structural of tb_ALU is
+
+   component ALU
+   port ( A          : in  std_logic_vector(7 downto 0);
+          B          : in  std_logic_vector(7 downto 0);
+          FN         : in  std_logic_vector(3 downto 0);
+          result     : out std_logic_vector(7 downto 0);
+          overflow   : out std_logic;
+          sign       : out std_logic
+        );
+   end component;
+
+   signal A          : std_logic_vector(7 downto 0);
+   signal B          : std_logic_vector(7 downto 0);
+   signal FN         : std_logic_vector(3 downto 0);
+   signal result     : std_logic_vector(7 downto 0);
+   signal overflow   : std_logic;
+   signal sign       : std_logic;
+   
+   constant period   : time := 2500 ns;
+
+begin  -- structural
+   
+   DUT: ALU
+   port map ( A         => A,
+              B         => B,
+              FN        => FN,
+              result    => result,
+              sign      => sign,
+              overflow  => overflow
+            );
+   
+   -- *************************
+   -- User test data pattern
+   -- *************************
+   
+   A <= "00000101",                    -- A = 5
+        "00001001" after 1 * period,   -- A = 9
+        "00010001" after 2 * period,   -- A = 17
+        "10010001" after 3 * period,   -- A = 145
+        "10010100" after 4 * period,   -- A = 148
+        "11010101" after 5 * period,   -- A = 213
+        "00100011" after 6 * period,   -- A = 35
+        "11110010" after 7 * period,   -- A = 242
+        "00110001" after 8 * period,   -- A = 49
+        "01010101" after 9 * period;   -- A = 85
+  
+   B <= "00000011",                    -- B = 3
+        "00000011" after 1 * period,   -- B = 3
+        "10010001" after 2 * period,   -- B = 145
+        "01111100" after 3 * period,   -- B = 124
+        "11111001" after 4 * period,   -- B = 249
+        "01101001" after 5 * period,   -- B = 105
+        "01100011" after 6 * period,   -- B = 35
+        "01101000" after 7 * period,   -- B = 104
+        "00101101" after 8 * period,   -- B = 45
+        "00100100" after 9 * period;   -- B = 36
+     
+   FN <= "0000",                              -- Pass A
+         "0001" after 1 * period,             -- Pass B
+         "0000" after 2 * period,             -- Pass A
+         "0001" after 3 * period,             -- Pass B
+         "0010" after 4 * period,             -- Pass unsigned A + B
+         "0011" after 5 * period,             -- Pass unsigned A - B  
+         "0011" after 6 * period,             -- Pass unsigned A - B
+         "0010" after 7 * period,             -- Pass unsigned A + B
+         "0011" after 8 * period,             -- Pass unsigned A - B
+--         "0100" after 9 * period,             -- Pass unsigned max(A, B)
+         "1010" after 10 * period,            -- Pass signed A + B
+         "1011" after 11 * period,            -- Pass signed A - B
+--         "0100" after 12 * period,            -- Pass signed max(A, B)
+         "1111" after 13 * period;            -- Invalid input command
+
+end structural;
---
+++ EITF35_Lab3_ALU/Lab3/Dev/DesignSources/ALU_top.vhd
@@ -0,0 +1,160 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+library work;
+use work.ALU_components_pack.all;
+
+entity ALU_top is
+   port ( clk        : in  std_logic;
+          reset      : in  std_logic;
+          b_Enter    : in  std_logic;
+          b_Sign     : in  std_logic;
+          input      : in  std_logic_vector(7 downto 0)
+          --seven_seg  : out std_logic_vector(6 downto 0);
+          --anode      : out std_logic_vector(3 downto 0)
+        );
+end ALU_top;
+
+architecture structural of ALU_top is
+
+-- component declarations
+component ALU_ctrl is
+   port ( clk     : in  std_logic;
+          reset   : in  std_logic;  -- connected to CPU reset button on FPGA board
+          enter   : in  std_logic;  -- connected to BTNC on FPGA board
+          sign    : in  std_logic;  -- connected to BTNL button on FPGA board
+          FN      : out std_logic_vector (3 downto 0);   -- ALU functions
+          RegCtrl : out std_logic_vector (1 downto 0)   -- Register update control bits
+        );
+end component;
+
+component ALU is
+   port ( A          : in  std_logic_vector (7 downto 0);   -- Input A
+          B          : in  std_logic_vector (7 downto 0);   -- Input B
+          FN         : in  std_logic_vector (3 downto 0);   -- ALU functions provided by the ALU_Controller (see the lab manual)
+          result 	 : out std_logic_vector (7 downto 0);   -- ALU output (unsigned binary)
+	      overflow   : out std_logic;                       -- '1' if overflow ocurres, '0' otherwise 
+	      sign       : out std_logic                        -- '1' if the result is a negative value, '0' otherwise
+        );
+end component;
+
+component regUpdate is
+   port ( clk        : in  std_logic;
+          reset      : in  std_logic;
+          RegCtrl    : in  std_logic_vector (1 downto 0);   -- Register update control from ALU controller
+          input      : in  std_logic_vector (7 downto 0);   -- Switch inputs
+          A          : out std_logic_vector (7 downto 0) ;   -- Input A
+          B          : out std_logic_vector (7 downto 0)    -- Input B
+        );
+end component;
+
+-- binary2BCD converter
+component binary2BCD is
+   generic ( WIDTH : integer := 8   -- 8 bit binary to BCD
+           );
+   port ( binary_in : in  std_logic_vector(WIDTH-1 downto 0);  -- binary input width
+          BCD_out   : out std_logic_vector(9 downto 0)        -- BCD output, 10 bits [2|4|4] to display a 3 digit BCD value when input has length 8
+        );
+end component;
+
+
+component seven_seg_driver is
+   port ( clk           : in  std_logic;
+          reset         : in  std_logic;
+          BCD_digit     : in  std_logic_vector(9 downto 0);          
+          sign          : in  std_logic;
+          overflow      : in  std_logic;
+          DIGIT_ANODE   : out std_logic_vector(3 downto 0);
+          SEGMENT       : out std_logic_vector(6 downto 0)
+        );
+end component;
+
+   -- SIGNAL DEFINITIONS
+   signal Enter, Sign : std_logic;
+   signal FN_ctrl : std_logic_vector(3 downto 0) ;
+   signal RegCtrl_signal : std_logic_vector(1 downto 0);
+   signal A_input, B_input : std_logic_vector( 7 downto 0 );
+   
+   -- signals for testing
+   signal tb_result : std_logic_vector ( 7 downto 0) := (others => '0') ;
+   signal tb_overflow, tb_sign : std_logic := '0';
+   signal tb_bcd_out : std_logic_vector(9 downto 0) := "0000000000";
+   signal board_DIGIT_ANODE : std_logic_vector(3 downto 0) := "1111";
+   signal board_SEGMENT : std_logic_vector( 6 downto 0) := "1111111";
+   
+
+begin
+
+    -- On - board buttons connections
+  
+    
+    
+    
+   ---- to provide a clean signal out of a bouncy one coming from the push button
+   ---- input(b_Enter) comes from the pushbutton; output(Enter) goes to the FSM 
+   debouncer1: debouncer
+   port map ( clk          => clk,
+              reset        => reset,
+              button_in    => b_Enter,
+              button_out   => Enter
+            );
+   
+    debouncer_sign: debouncer
+         port map ( clk          => clk,
+                    reset        => reset,
+                    button_in    => b_sign,
+                    button_out   => Sign
+                       );
+
+   -- ****************************
+   -- DEVELOPE THE STRUCTURE OF ALU_TOP HERE
+   -- ****************************
+   
+   ALU_Controller: ALU_ctrl
+   port map (
+               clk => clk,
+               reset => reset,
+               enter => Enter,          -- connected to out of debouncer
+               sign => Sign,
+               FN => FN_ctrl,
+               RegCtrl => RegCtrl_signal
+               );
+    
+   register_update: regUpdate
+    port map (
+                clk => clk,
+                reset => reset,
+                RegCtrl => RegCtrl_signal,
+                input => input,                   -- from Switch positions ( XDC file )
+                A => A_input,
+                B => B_input
+                );
+               
+   ALU_block : ALU
+   port map (
+                A => A_input,
+                B => B_input,
+                FN => FN_ctrl,
+                result => tb_result,
+                overflow => tb_overflow,
+                sign => tb_sign                
+                );
+       
+   Bin2BCD_block : binary2BCD
+   port map (
+               binary_in => tb_result,
+               BCD_out => tb_bcd_out
+              );
+seg: seven_seg_driver
+   port map ( 
+          clk => clk,
+          reset => reset,
+          BCD_digit => tb_bcd_out,          
+          sign  => tb_sign,
+          overflow  => tb_overflow,
+          DIGIT_ANODE => board_DIGIT_ANODE,
+          SEGMENT   => board_SEGMENT
+        );
+   
+
+end structural;
