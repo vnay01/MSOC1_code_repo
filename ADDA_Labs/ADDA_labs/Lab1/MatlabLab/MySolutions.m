02198ca510db96a39a79097de7351a869fa7452c
Vinay Singh <vnay01@gmail.com>
2021-12-06 15:08:09 +0100

    push#2
    Issues remain for ex 12.
    Pending 13 to 17

--- ADDA_Labs/ADDA_labs/Lab1/MatlabLab/MySolutions.m
+++ ADDA_Labs/ADDA_labs/Lab1/MatlabLab/MySolutions.m
@@ -1,4 +1,5 @@
 function output = MySolutions(varargin);
+
 %
 % ETI220 - Integrated A/D and D/A Converters
 %
@@ -56,9 +57,12 @@ end
 % being spread across each spectral line.
 
 if ex==1
-  % Set rectangular window
+  clf;
+    % Set rectangular window
+  
   win   = 'rect';
   % Generate and sample signal
+
   x     = sampling('signal','sine','fin',fin,'fs',fs,'ain',A1,'samples',len);
 %  a     = sampling('signal','sine','fin',2e6, 'fs',20e6,'ain',A1,'samples',len);
   % Make FFT
@@ -208,7 +212,7 @@ if ex==5
   fprintf("%.6f ",mean(spec));
  
   % FFT with averaging = 0 
-  % The last FFT of the signal is output
+  % Only the last FFT of the signal is output
   nexttile
   means = 0;
   spec = adcfft('d',y,'skip',1,'mean',means,'N',Nx,'w',win);
@@ -237,6 +241,8 @@ end
 if ex==6
     % Put YOUR OWN ORIGINAL solution here!
     fin = 9.97e12;
+    win = 'hann1';
+    
  x = sampling('signal','sine','fin',fin,'fs',fs,'ain',A1,'samples', len,'k3',0.01);
  spec = adcfft('d',x.data,'skip',1,'mean',means,'N',Nx,'w',win);
  % change input frequency to 39.19 MHz
@@ -265,7 +271,7 @@ end
 if ex ==7
     cla;
   
-  
+    win = 'hann1';
     Cs = 0.01e-12:0.2e-12:10.0e-12 ;  % range of Cs 0.01 pf to 10.0 pf - steps : 0.2 pf
 %     g = length(Cs);
 %     fprintf("%d",g);
@@ -319,7 +325,7 @@ end
 % end
 % Pending : Plotting a straight line for calculated SNRs
 end
-toc
+
 
 %{
 Block works... Plotting needs to be beautified!!
@@ -329,57 +335,223 @@ Block works... Plotting needs to be beautified!!
 
 % Exercise 8    -- Study effect of clock jitter.
 if ex==8
+
     % Put YOUR OWN ORIGINAL solution here!
-    %fin = [9.97e6 10.09e6 1021e6 ];
+    win = 'hann1';
+    % come up with come algo for generating fin as prime numbers for better
+    % simulation range!!
     fin    = [9.97e6 10.09e6 10.13e6 11.17e6 ];
-%     fs = [];
+    
     for j = 1:1:2 
     fs = fin(j).*(8192/(fin(j).*100));
     end
-    
+    jit_gaus = [1e-15 5e-15 15e-15];     % jitter deviation : 1ps 5ps 15ps
     Cs = 0.01e-12;
- %sampling('signal','sine','fin',fin,'fs',fs,'ain',A1*0.97,'samples', len,'k2',sk2,'k3',sk3,'k4',sk4,'k5',sk5,'Cs',Cs,'Rs',Rs,'jit_gaus',jit_gaus);
-  for k =1:1:4 
-  sampled_signal =  sampling('signal','sine','fin',fin(k),'fs',fs,'ain',A1,'samples', len,'Cs',Cs);
+
+    %sampling('signal','sine','fin',fin,'fs',fs,'ain',A1*0.97,'samples', len,'k2',sk2,'k3',sk3,'k4',sk4,'k5',sk5,'Cs',Cs,'Rs',Rs,'jit_gaus',jit_gaus);
+ 
+   figure(8)
+   tiledlayout(4,1)
+   nexttile
+  for m = 1:1:3 
+    for k =1:1:4 
+  sampled_signal =  sampling('signal','sine','fin',fin(k),'fs',fs,'ain',A1,'samples', len,'Cs',Cs,'jit_gaus',jit_gaus(m));
   signal_fft    = adcfft('d',sampled_signal.data,'skip',1,'mean',means,'N',Nx,'w',win);
   signal_perf   = adcperf('data',signal_fft,'snr','sndr','w',win);
   snr_signal(k)    = signal_perf.snr;
   sndr_signal(k)    = signal_perf.sndr;
-  end
-  % plot starts here
-  figure(8);
-  tiledlayout(2,1)
-  nexttile
-  plot(0:length(signal_fft)-1,20*log10(abs(signal_fft)))
-
-  nexttile
-  plot(fin,snr_signal,'k.-','LineWidth',2)
+  plot(0:length(signal_fft)-1,20*log10(abs(signal_fft)),'r')
   hold on
-  plot(fin,sndr_signal,'rd')
+
+      end
  
+  end
+  
+  xlabel("Frequency");
+  ylabel("PSD");
+
+
+%   axis([0 length(signal_fft)-1 ])
+    nexttile
+  plot(fin,snr_signal,'k.-','LineWidth',2)
+  xlabel("Frequency");
+  ylabel("SNR");
+  hold on
+nexttile
+  plot(fin,sndr_signal,'kd-')
+   xlabel("Frequency");
+  ylabel("SNDR");
+%{
+  xpos = [fin];
+ ypos = [sndr_signal];
+ lbl = ['x','y','d','f'];
+ for t = 1:1:4
+text(xpos(t),ypos(t),lbl(k))
+ end
+ %}
+  
   hold off
   fprintf(newline);
-fprintf("%.10f \t %.10f",snr_signal, sndr_signal);
+  fprintf("%.4f",snr_signal);
+  fprintf(newline);
+  fprintf("%.4f",sndr_signal);
+  fprintf(newline);
+ 
+%{
+  Theoretical calculation and plot pending.
+  %}
 end
 
-% Exercise 9
-if ex==9
+% Exercise 9  -- Effect of clock jitter
+if ex==9  
+    cla;
     % Put YOUR OWN ORIGINAL solution here!
+    win = 'hann1';
+    fs = 250e6;
+%     fin = (3127./8192).*fs;     % required to avoid spectral leakage due to windowing.
+   
+    fin = 100e6;        % uncomment for non-coherent sampling    
+    figure(9)
+    tiledlayout(2,1)
+    nexttile
+    for jit_gaus = 1e-15:1e-15:20e-12    
+         sampled_signal =  sampling('signal','sine','fin',fin,'fs',fs,'ain',A1,'samples', len,'jit_gaus',jit_gaus);
+         signal_fft    = adcfft('d',sampled_signal.data,'skip',1,'mean',means,'N',Nx,'w',win);
+         signal_perf   = adcperf('data',signal_fft,'sndr','w',win);
+         plot(jit_gaus,signal_perf.sndr,'r.')
+         hold on
+         
+         
+    end
+    nexttile
+    plot(0:length(signal_fft)-1,10*log10(abs(signal_fft)))
+    xlabel("jitter_deviation");
+    ylabel("SNDR");
+    hold off
+    
 end
 
-% Exercise 10
+% A study in quantization %
+% Exercise 10       -- quantization noise modelling as a rectangular PSD
 if ex==10
     % Put YOUR OWN ORIGINAL solution here!
+    cla;
+    R = 8;
+    win = 'hann1';
+    sampled_signal = sampling('signal','sine','fin',fin,'fs',fs,'ain',A1,'samples', len);
+%     added_noise = Arndn*randn(size(sampled_signal.data));       % adding gaussian noise with same amp. of 8 bit Quantized Noise.
+%     noised_signal = sampled_signal.data + added_noise;
+    
+    signal_fft = adcfft('d',sampled_signal.data,'skip',1,'mean',means,'N',Nx,'w',win);
+    signal_perf   = adcperf('data',signal_fft,'sndr','w',win);
+    
+    % plot starts here
+    figure(10)
+    tiledlayout(2,1)
+    nexttile
+    plot(0:length(signal_fft)-1,120*log10(abs(signal_fft)))
+    xlabel("Frequency");
+    ylabel("PSD : Signal");
+    fprintf(newline);
+    fprintf("Calculated SNDR : %.4f",signal_perf.sndr);
+    fprintf(newline);
+    
+    nexttile
+    % effect of averaging
+    means = 1:1:16;
+    for k = 1:1:16
+        signal_fft = adcfft('d',sampled_signal.data,'skip',1,'mean',means(k),'N',Nx,'win',win);
+        signal_perf   = adcperf('data',signal_fft,'sndr','w',win);
+        fprintf("%d \t ",k);
+        plot(0:length(signal_fft)-1,120*log10(abs(signal_fft)))
+        hold on
+    fprintf(newline);
+    fprintf("Calculated SNDR : %.4f \t",signal_perf.sndr);
+    end
+    
+    xlabel("Frequency");
+    ylabel("PSD : Signal");
+    
+    
+    
 end
 
 % Exercise 11
 if ex==11
     % Put YOUR OWN ORIGINAL solution here!
+       R = 8;
+    win = 'hann1';
+%     npow =  A1; 
+    sampled_signal = sampling('signal','sine','fin',fin,'fs',fs,'ain',A1,'samples', len);
+    added_noise = A1*randn(size(sampled_signal.data));
+    noised_signal = sampled_signal.data + added_noise;
+    quantized_sig = quantization('data',noised_signal,'R',R,'vref',1,'npow', npow);
+    signal_fft = adcfft('d',quantized_sig,'skip',1,'mean',means,'N',Nx,'w',win);
+    signal_perf   = adcperf('data',signal_fft,'sndr','w',win);
+    
+    % plot starts here
+    figure(11)
+    tiledlayout(2,1)
+    nexttile
+    plot(0:length(signal_fft)-1,120*log10(abs(signal_fft)))
+    
+    fprintf(newline);
+    fprintf("Calculated SNDR : %.4f",signal_perf.sndr);
+    fprintf(newline);
+    
+    nexttile
+    % effect of averaging
+    means = 1:1:64;
+    for k = 1:1:64
+        signal_fft = adcfft('d',quantized_sig,'skip',1,'mean',means(k),'N',Nx,'win',win);
+        signal_perf   = adcperf('data',signal_fft,'sndr','w',win);
+        plot(0:length(signal_fft)-1,120*log10(abs(signal_fft)))
+        hold on
+    fprintf(newline);
+    fprintf("Calculated SNDR : %.4f \t",signal_perf.sndr);
+    end
+   
 end
 
-% Exercise 12
+% Exercise 12  -- 
 if ex==12
     % Put YOUR OWN ORIGINAL solution here!
+    % generate a ramp singnal [-1V to 1V] - using unit function
+    clf;
+    win = 'hann1';
+%    len = Nx * means = 8192 * 16 = 131072 pts.
+    sampled_signal = sampling('signal','ramp','fin',fin,'fs',fs,'ain',A1,'samples', len);
+    quantized_sig = quantization('data',sampled_signal.data,'R',12,'vref',1,'npow', npow); 
+    signal_fft = adcfft('d',quantized_sig,'skip',1,'mean',means,'N',Nx,'w',win);
+    
+    figure(13);
+    plot(0:length(signal_fft)-1,20*log10(signal_fft),'k-');
+    
+%{
+    fin = 9.97e6;
+    fs = 81.92e6;
+  t = 0:(1/fs):((10*1/fin));
+  g = A1*sin(2*pi*fin*t);
+  figure(12);
+  plot(t,g,'r-')
+  grid on
+
+    %}
+
+%     sampled_signal2 = sampling('signal','sine','fin',fin,'fs',fs,'ain',A1,'samples',len);
+%     figure(12);
+%     tiledlayout(2,1)
+%     nexttile
+%     plot(sampled_signal,'k-')
+%     nexttile
+%     plot(sampled_signal2,'r-')
+%     
+ 
+%     plot((1/fs).*(0:len-1),ramp_sig(t));
+
+    
+    % sampling & quantization
+   
 end
 
 % Exercise 13
